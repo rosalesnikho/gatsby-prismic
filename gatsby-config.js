cc1bf7e7d0b2aa2cb528200bc13d826e2d3928f8
nikvicros <nikho.rosales@gmail.com>
2019-09-28 13:08:45 -0500

    added google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
-            trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+            trackingId: "UA-148958847-1",
             head: true,
         },
     },
